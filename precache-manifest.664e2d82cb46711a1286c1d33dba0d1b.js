d161d601a5c50ce17b13555fefa9f1bd78adcb9d
Fadhil <fadhil.akins@gmail.com>
2020-06-26 11:54:16 +0100

    Updates

---
+++ precache-manifest.664e2d82cb46711a1286c1d33dba0d1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b16ff82244d0a122f9587f9f08bcc3",
+    "url": "/TheCount511/Markdown-Preview/index.html"
+  },
+  {
+    "revision": "c3042b70a13ff42a6e91",
+    "url": "/TheCount511/Markdown-Preview/static/css/main.3069074a.chunk.css"
+  },
+  {
+    "revision": "e27200544e961320ad70",
+    "url": "/TheCount511/Markdown-Preview/static/js/2.66f51fdc.chunk.js"
+  },
+  {
+    "revision": "c3042b70a13ff42a6e91",
+    "url": "/TheCount511/Markdown-Preview/static/js/main.cdd9d342.chunk.js"
+  },
+  {
+    "revision": "d193367fca978b4fbff4",
+    "url": "/TheCount511/Markdown-Preview/static/js/runtime-main.6e6991b0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/TheCount511/Markdown-Preview/static/media/logo.25bf045c.svg"
+  }
+]);
